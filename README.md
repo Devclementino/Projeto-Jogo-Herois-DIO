67ee93187e5bcaac6de6d33e54650d110c3b1fee
clementino lima <clementinodev@gmail.com>
2023-11-15 17:36:11 -0300

    inserido README.md index.js

---
+++ README.md
@@ -0,0 +1,15 @@
+# Projeto de Jogo de Heróis
+
+Bem-vindo ao projeto do jogo de heróis! Este é um jogo simples escrito em JavaScript.
+
+## Links Importantes
+
+- [Modelo no Figma] https://www.figma.com
+
+## Configuração do Projeto
+
+Certifique-se de configurar corretamente o projeto antes de executá-lo. Siga as instruções no [arquivo de configuração](config/README.md).
+
+## Executando o Projeto
+
+Para executar o projeto, siga as instruções no [guia de instalação](install/README.md).
---
+++ index.js
@@ -0,0 +1,41 @@
+class Heroi {
+
+  constructor(nome, idade, tipo) {
+    this.nome = nome
+    this.idade = idade
+    this.tipo = tipo
+  }
+
+  atacar() {
+    let ataque;
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia"
+        break;
+      case "guerreiro":
+        ataque = "espada"
+        break;
+      case "monge":
+        ataque = "artes marciais"
+        break;
+      case "ninja":
+        ataque = "espada"
+        break;
+      default:
+        ataque = "um ataque genérico"
+    }
+
+    console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`)
+  }
+}
+
+const heroiMago = new Heroi("Merlin", 104, "mago")
+const heroiGuerreiro = new Heroi("Aragorn", 50, "guerreiro")
+const heroiMonge = new Heroi("Bruce Lee", 40, "monge")
+const heroiNinja = new Heroi("Hanzo", 38, "ninja")
+
+heroiMago.atacar()
+heroiGuerreiro.atacar()
+heroiMonge.atacar()
+heroiNinja.atacar()
+
